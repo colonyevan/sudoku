fac202c4b35d7779b96d1cf29201e02ae2f4c59e
evancolo <colony.evan@gmail.com>
2020-04-29 21:28:57 -0500

    Finished the backtracking

--- sudoku.py
+++ sudoku.py
@@ -3,13 +3,17 @@
 from random import choice
 import re
 
-exp = re.compile('\d*')
+exp = re.compile('([0-9]*\.*)*')
 
 puzzles = [
-     '9.2.5..8.9..21..35753.869.....3.....3...7...1.....4.....719.45626..45..9.4..3.1.8']
+     '53..7....6..195....98....6.8...6...34..8.3..17...2...6.6....28....419..5....8..79']
 
+class Solved(Exception):
+    pass
 
 class Puzzle:
+    grid = [[] for i in range(9)]
+
     def __init__(self):
         """Initalizes the grid to a proper string"""
         self.readPuzzle()
@@ -31,31 +35,86 @@ class Puzzle:
 
         while len(data) != 81 or exp.fullmatch(data) is None:
             data = input(
-                'You didn\'t enter a valid string. please enter 81 integers: ')
+                'You didn\'t enter a valid string. please enter 81 integers or deciamls: ')
 
         counter = 0
 
-        for i in data:
-            self.grid[counter / 9][counter % 9] = i
+        for num in data:
+            self.grid[int(counter / 9)].append(num)
             counter += 1
 
+        print('\n')
         return
 
     def printGrid(self):
         """Prints the sudoku grid"""
-        for i in self.i:
-            for j in self.i[j]:
-                print(f'{self.grid[i][j]} f')
-            print('\n')
+        for row, i in enumerate(self.grid):
+            for col, j in enumerate(i):
+                print(j, end = " ")
+                if ((col + 1) % 3 == 0 and col is not 8):
+                    print('|', end=" ")
+            print('\n', end = "")
+            if (row + 1) % 3 == 0 and row is not 8:
+                print('------+-------+-------')
 
     def getRandom(self):
-        """Get a random Sudoku string"""
+        """Gets a random Sudoku string"""
         return choice(puzzles)
 
+    def backtrack(self, row, col):
+        """Uses backtracking to figure out the answer to a puzzle"""
+        # If it is already filled, means it was given, skip
+        if row == 9 and self.valid():
+            print("Solved!")
+            self.printGrid()
+            raise Solved
+        # Else, start the loop here
+        elif self.grid[row][col].isdigit():
+            if col == 8:
+                self.backtrack(row + 1, 0)
+            else:
+                self.backtrack(row, col + 1)
+        else:
+            for num in range(1, 10):
+                self.grid[row][col] = str(num)
+                if self.valid():
+                    if col == 8:
+                        self.backtrack(row + 1, 0)
+                    else:
+                        self.backtrack(row, col + 1)
+                self.grid[row][col] = '.'
+        return
+    
+    def valid(self):
+        """Figures out if the current board is valid"""
+        col_items = [set() for i in range(9)]
+        box_items = [set() for i in range(9)]
+
+        for row in range(9):
+            row_items = set()
+            for col in range(9):
+                if self.grid[row][col].isdigit():
+                    item = self.grid[row][col]
+                    if item in row_items or item in col_items[col]:
+                        return False
+                    row_items.add(item)
+                    col_items[col].add(item)
+
+                    index = (row // 3) * 3 + col // 3
+                    if item in box_items[index]:
+                        return False
+                    box_items[index].add(item)
+        return True
 
 def runner():
+    """Runs the Puzzle Object"""
     obj = Puzzle()
     obj.printGrid()
-
+    try:
+        obj.backtrack(0, 0)
+    except Solved:
+        return True
+    else:
+        return False
 
 runner()
