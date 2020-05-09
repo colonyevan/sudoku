3fed7042bac5dec1495e68a6b046f5d82618059d
evancolo <colony.evan@gmail.com>
2020-05-09 14:23:01 -0500

    Working on pygame

--- sudoku.py
+++ sudoku.py
@@ -1,7 +1,31 @@
 """A program that solves a puzzle using backtracking"""
-
 from random import choice
 import re
+import os
+import pygame
+import sys
+from pygame.locals import *
+
+# Setting the game FPS
+FPS = 10
+
+# Global Window Size Vars
+WINDOWMULTIPLIER = 5 
+WINDOWSIZE = 81
+WINDOWWIDTH = WINDOWSIZE * WINDOWMULTIPLIER
+WINDOWHEIGHT = WINDOWSIZE * WINDOWMULTIPLIER
+SQUARESIZE = int((WINDOWSIZE * WINDOWMULTIPLIER) / 3)
+CELLSIZE = int(SQUARESIZE / 3)
+
+# Font Setting
+global BASICFONT, BASICFONTSIZE
+BASICFONTSIZE = 15
+BASICFONT = pygame.font.Font('freesansbold.ttf', BASICFONTSIZE)
+
+# Colors
+WHITE = (255, 255, 255)
+LIGHTGRAY = (200, 200, 200)
+BLACK = (0, 0, 0)
 
 exp = re.compile('([0-9]*\.*)*')
 
@@ -11,14 +35,14 @@ puzzles = [
 class Solved(Exception):
     pass
 
-class Puzzle:
+class Puzzle(object):
     grid = [[] for i in range(9)]
 
-    def __init__(self):
+    def __init__(self) -> None:
         """Initalizes the grid to a proper string"""
         self.readPuzzle()
 
-    def readPuzzle(self):
+    def readPuzzle(self) -> None:
         """Asks user for Sudoku string if none specified"""
         userInput = input('Generated (G) grid or User (U) specified? ')
 
@@ -46,22 +70,22 @@ class Puzzle:
         print('\n')
         return
 
-    def printGrid(self):
+    def printGrid(self) -> None:
         """Prints the sudoku grid"""
         for row, i in enumerate(self.grid):
             for col, j in enumerate(i):
                 print(j, end = " ")
-                if ((col + 1) % 3 == 0 and col is not 8):
+                if ((col + 1) % 3 == 0 and col != 8):
                     print('|', end=" ")
             print('\n', end = "")
-            if (row + 1) % 3 == 0 and row is not 8:
+            if (row + 1) % 3 == 0 and row != 8:
                 print('------+-------+-------')
 
-    def getRandom(self):
+    def getRandom(self) -> str:
         """Gets a random Sudoku string"""
         return choice(puzzles)
 
-    def backtrack(self, row, col):
+    def backtrack(self, row, col) -> None:
         """Uses backtracking to figure out the answer to a puzzle"""
         # If it is already filled, means it was given, skip
         if row == 9 and self.valid():
@@ -85,7 +109,7 @@ class Puzzle:
                 self.grid[row][col] = '.'
         return
     
-    def valid(self):
+    def valid(self) -> bool:
         """Figures out if the current board is valid"""
         col_items = [set() for i in range(9)]
         box_items = [set() for i in range(9)]
@@ -106,15 +130,52 @@ class Puzzle:
                     box_items[index].add(item)
         return True
 
-def runner():
-    """Runs the Puzzle Object"""
-    obj = Puzzle()
-    obj.printGrid()
-    try:
-        obj.backtrack(0, 0)
-    except Solved:
-        return True
-    else:
-        return False
+def drawGrid() -> None:
+    # Draw Minor Lines
+    for x in range(0, WINDOWWIDTH, CELLSIZE): # draw vertical lines
+        pygame.draw.line(DISPLAYSURF, LIGHTGRAY, (x,0),(x,WINDOWHEIGHT))
+    for y in range (0, WINDOWHEIGHT, CELLSIZE): # draw horizontal lines
+        pygame.draw.line(DISPLAYSURF, LIGHTGRAY, (0,y), (WINDOWWIDTH, y))
+    
+    # Draw Major Lines
+    for x in range(0, WINDOWWIDTH, SQUARESIZE): # draw vertical lines
+        pygame.draw.line(DISPLAYSURF, BLACK, (x,0),(x,WINDOWHEIGHT))
+    for y in range (0, WINDOWHEIGHT, SQUARESIZE): # draw horizontal lines
+        pygame.draw.line(DISPLAYSURF, BLACK, (0,y), (WINDOWWIDTH, y))
+    return None
 
-runner()
+def runner() -> bool:
+    global FPSCLOCK, DISPLAYSURF
+    pygame.init()
+    FPSCLOCK = pygame.time.Clock()
+    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH,WINDOWHEIGHT))
+
+    # Mouse variables
+    mouseClicked = False
+    mousex = 0
+    mousey = 0
+
+    # Setting up the grid
+    pygame.display.set_caption('Sudoku Solver')
+    DISPLAYSURF.fill(WHITE)
+    drawGrid()
+
+    while True: #main game loop
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == MOUSEMOTION:
+                mousex, mousey = event.pos
+            elif event.type == MOUSEBUTTONUP:
+                mousex, mousey = event.pos
+                mouseClicked = True
+        
+        if mouseClicked:
+            drawBox(mousex, mousey)
+
+        pygame.display.update()    
+        FPSCLOCK.tick(FPS)
+
+if __name__ == "__main__":
+    runner()
